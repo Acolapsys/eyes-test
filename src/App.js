055382ed7da4ef449d1cd842903f2105116eba8b
Acolapsys <timur_sh@bk.ru>
2022-09-15 19:09:32 +0300

    refactor, add controls, add saving to image

---
+++ src/App.js
@@ -0,0 +1,186 @@
+const colors = Object.freeze({
+  ON: "#fff",
+  OFF: "#222"
+});
+
+const buttonTitle = Object.freeze({
+  START: "Старт",
+  PAUSE: "Пауза",
+  CLEAR: "Новое поле"
+});
+
+export default class App {
+  constructor({
+    cols = 50,
+    rows = 50,
+    highlightTime = 500,
+    pauseTime = 1000
+  } = {}) {
+    this.colsCount = cols;
+    this.rowsCount = rows;
+    this.highlightTime = highlightTime;
+    this.pauseTime = pauseTime;
+    this.$table = document.querySelector(".table");
+    this.$startButton = document.querySelector(".start-button");
+    this.$clearButton = document.querySelector(".clear-button");
+    this.$counter = document.querySelector(".counter");
+    this.$colsCount = document.querySelector("#cols");
+    this.$rowsCount = document.querySelector("#rows");
+    this.$highlightTime = document.querySelector("#highlightTime");
+    this.$pauseTime = document.querySelector("#pauseTime");
+    this.$save = document.querySelector(".save");
+
+    this.testArr = [];
+    this.intervalId = null;
+    this.timeoutId = null;
+    this.init();
+  }
+
+  init() {
+    this.setElTitle(this.$startButton, buttonTitle.START);
+    this.setElTitle(this.$clearButton, buttonTitle.CLEAR);
+
+    this.setElValue(this.$colsCount, this.colsCount);
+    this.setElValue(this.$rowsCount, this.rowsCount);
+    this.setElValue(this.$highlightTime, this.highlightTime);
+    this.setElValue(this.$pauseTime, this.pauseTime);
+    this.initListeners();
+  }
+
+  get stepTime() {
+    return +this.pauseTime + +this.highlightTime;
+  }
+
+  get leftCount() {
+    return this.testArr?.length;
+  }
+
+  setElTitle($el, title) {
+    $el.innerHTML = title;
+  }
+
+  setElValue($el, value) {
+    $el.value = value;
+  }
+
+  highLightCell($cell) {
+    if ($cell) {
+      $cell.style.background = colors.ON;
+    }
+  }
+
+  unHighLightCell($cell) {
+    if ($cell) {
+      $cell.style.background = colors.OFF;
+    }
+  }
+
+  generateTable() {
+    this.testArr = this.generateTestArray();
+    this.setElTitle(this.$counter, this.leftCount);
+    this.$table.innerHTML = "";
+    const makeCell = () => {
+      const $el = document.createElement("td");
+      $el.classList.add("cell");
+      return $el;
+    };
+    const cellsArray = Array.from(Array(this.rowsCount), () =>
+      Array.from(Array(this.colsCount), () => makeCell())
+    );
+
+    cellsArray.forEach((row, rowIndex) => {
+      const $row = document.createElement("tr");
+      row.forEach(($cell, cellIndex) => {
+        $row.append($cell);
+        $cell.setAttribute("data-x", cellIndex);
+        $cell.setAttribute("data-y", rowIndex);
+      });
+      this.$table.append($row);
+    });
+
+    const $circle = document.createElement("div");
+    $circle.classList.add("circle");
+    this.$table.append($circle);
+  }
+
+  generateTestArray() {
+    const arr = [];
+    for (let i = 0; i < this.rowsCount; i++) {
+      for (let j = 0; j < this.colsCount; j++) {
+        arr.push({ x: i, y: j });
+      }
+    }
+    return arr;
+  }
+
+  startInterval() {
+    return setInterval(() => {
+      const idx = Math.floor(Math.random() * this.testArr.length - 1);
+
+      const $cell = this.$table.querySelector(
+        `[data-x="${this.testArr[idx].x}"][data-y="${this.testArr[idx].y}"]`
+      );
+      this.highLightCell($cell);
+      this.timeout = setTimeout(() => {
+        this.unHighLightCell($cell);
+      }, this.highlightTime);
+      this.testArr.splice(idx, 1);
+      this.setElTitle(this.$counter, this.leftCount);
+    }, this.stepTime);
+  }
+  stop() {
+    this.setElTitle(this.$startButton, buttonTitle.START);
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+  }
+
+  start() {
+    this.setElTitle(this.$startButton, buttonTitle.PAUSE);
+    this.intervalId = this.startInterval();
+  }
+
+  initListeners() {
+    this.$startButton.onclick = () => {
+      this.$startButton.blur();
+      if (this.intervalId) {
+        this.stop();
+      } else {
+        this.start();
+      }
+    };
+    this.$clearButton.onclick = () => {
+      this.generateTable(this.$table);
+    };
+    this.$save.onclick = () => {
+      this.saveImage();
+    };
+    this.$colsCount.onchange = (e) => {
+      this.colsCount = +e.target.value;
+    };
+    this.$rowsCount.onchange = (e) => {
+      this.rowsCount = +e.target.value;
+    };
+    this.$pauseTime.onchange = (e) => {
+      this.pauseTime = +e.target.value;
+    };
+    this.$highlightTime.onchange = (e) => {
+      this.highlightTime = +e.target.value;
+    };
+    window.addEventListener("keydown", () => {
+      if (this.timeout) {
+        clearTimeout(this.timeout);
+        this.timeout = null;
+      }
+    });
+  }
+  saveImage() {
+    window.domtoimage
+      .toJpeg(this.$table)
+      .then(function (dataUrl) {
+        const link = document.createElement("a");
+        link.download = "sample-test.jpeg";
+        link.href = dataUrl;
+        link.click();
+      });
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,85 +1,11 @@
-import { generateTable } from "./generate.js";
+import App from "./App.js";
 
-const $table = document.querySelector(".table");
-const $startButton = document.querySelector(".start-button");
-const $clearButton = document.querySelector(".clear-button");
-
-const onColor = "#fff";
-const offColor = "#222";
-
-let intervalId = null;
-let timeout = null;
-
-generateTable($table);
-
-const highLightCell = ($cell) => {
-  $cell.style.background = onColor;
+const config = {
+  cols: 50,
+  rows: 50,
+  highlightTime: 500,
+  pauseTime: 1000
 };
 
-const unHighLightCell = ($cell) => {
-  $cell.style.background = offColor;
-};
-const clearTable = (node) => {
-  for (let i = 0; i < node.childNodes.length; i++) {
-    let child = node.childNodes[i];
-    clearTable(child);
-    if (child.tagName === "td") {
-      unHighLightCell(child);
-    }
-  }
-};
-const generateTestArray = () => {
-  const arr = [];
-  for (let i = 0; i < 50; i++) {
-    for (let j = 0; j < 50; j++) {
-      arr.push({ x: i, y: j });
-    }
-  }
-  return arr;
-};
-
-const startInterval = () => {
-  const testArr = generateTestArray();
-  return setInterval(() => {
-    const idx = Math.floor(Math.random() * testArr.length - 1);
-
-    const $cell = $table.querySelector(
-      `[data-x="${testArr[idx].x}"][data-y="${testArr[idx].y}"]`
-    );
-    highLightCell($cell);
-    timeout = setTimeout(() => {
-      unHighLightCell($cell);
-    }, 500);
-    testArr.splice(idx, 1);
-    console.log(testArr.length);
-  }, 1500);
-};
-const stop = () => {
-  $startButton.innerHTML = "Start";
-  clearInterval(intervalId);
-  intervalId = null;
-};
-
-const start = () => {
-  $startButton.innerHTML = "Stop";
-  intervalId = startInterval();
-};
-
-$startButton.onclick = () => {
-  $startButton.blur();
-  if (intervalId) {
-    stop();
-  } else {
-    start();
-  }
-};
-
-$clearButton.onclick = () => {
-  generateTable($table);
-};
-window.addEventListener("keydown", () => {
-  if (timeout) {
-    clearTimeout(timeout);
-    timeout = null;
-  }
-});
+const app = new App(config);
+app.generateTable();
